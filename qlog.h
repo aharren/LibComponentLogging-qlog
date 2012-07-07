095ae9bec4d979cdfcbbde36aa6e9817c9f3b97e
Arne Harren <ah@0xc0.de>
2012-07-07 10:37:14 +0200

    update documentation

--- qlog.h
+++ qlog.h
@@ -29,13 +29,53 @@
 //
 // qlog just consists of this small header file which defines a short logging
 // macro for every log level of LibComponentLogging, e.g. qlerror() for error
-// messages and qltrace() for trace messages. Additionally, all logging macros
-// take the current log component from the ql_component preprocessor define
-// which can be (re)defined in your application at a file-level, section-based,
-// or global scope. If you want to include the log component in your logging
-// statements instead of using the ql_component define, you can use the _c
-// variants of the logging macros which take the log component as the first
-// argument, e.g. qlerror_c(lcl_cMain), qltrace_c(lcl_cMain, @"message").
+// messages and qltrace() for trace messages. All qlog macros support a format
+// string and arguments, e.g. qlerror(@"code %d", code).
+//
+// All logging macros take the current log component from the ql_component
+// preprocessor define which can be (re)defined in your application per file,
+// per section, or at global scope.
+//
+// If you want to include the log component in your logging statements instead
+// of using the ql_component define, you can use the _c variants of the qlog
+// macros which take the log component as the first argument, e.g.
+// qlerror_c(lcl_cMain), or qltrace_c(lcl_cMain, @"message").
+//
+// To install qlog, just copy the qlog.h header file to your project and add
+// an import of qlog.h to your prefix header file or to your LibComponentLogging
+// extensions configuration file lcl_config_extensions.h:
+//
+//   #import "qlog.h"
+//
+// Then, define the preprocessor symbol ql_component at a global scope with your
+// default log component, e.g. add a define to your prefix header file:
+//
+//   #define ql_component lcl_cDefaultLogComponent
+//
+// Now, logging statements can be added to your application by simply using the
+// qlog macros instead of LibComponentLogging's lcl_log macros:
+//
+//   qlinfo(@"initialized");
+//   qlerror(@"file '%@' does not exist", file);
+//   qltrace();
+//
+// All these logging statements will use the log component from the ql_component
+// define which is visible at the location of the logging statement.
+//
+// If you want to use a specific log component for all logging statements in a
+// file, you can simply redefine ql_component to match this log component, e.g.
+// by adding a #undef #define sequence at the top of the file, e.g.
+//
+//   #undef ql_component
+//   #define ql_component lcl_cFileLevelComponent
+//
+// If you want to use a specific log component at a specific location in your
+// code, you can use the _c variants of the macros which take the log component
+// as the first argument, e.g.
+//
+//  qlinfo_c(lcl_cMain, @"initialized");
+//  qlerror_c(lcl_cMain, @"file '%@' does not exist", file);
+//  qltrace_c(lcl_cMain);
 //
 
 
